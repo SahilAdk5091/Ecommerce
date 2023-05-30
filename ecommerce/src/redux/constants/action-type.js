706ec4391990d6e9caafc21a6a925735a21834e5
SahilAdk5091 <sahiladk234@gmail.com>
2023-05-30 20:20:04 +0545

    Adding Redux store and components

---
+++ ecommerce/src/redux/constants/action-type.js
@@ -0,0 +1,7 @@
+export const ActionTypes = {
+    SET_PRODUCTS : "SET_PRODUCTS",
+    SELECTED_PRODUCT: "SELECTED_PRODUCT",
+    RMOVE_SELECTED_PRODUCTS: "REMOVE_SELECTED_PRODUCTS",
+}
+
+export default ActionTypes
